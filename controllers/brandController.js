8c4975a32b94bd3c80aef1041cfa332cd6ae8885
jonathanaloya <alo23004@byui.edu>
2024-10-17 20:23:28 +0300

    added function for the brand and brand ID

---
+++ controllers/brandController.js
@@ -0,0 +1,15 @@
+// Function to get a brand by brand and brandID
+const getBrandByBrandId = async (req, res) => {
+    const { brand, brandId } = req.params;
+};
+
+// Function to add a brand by brand and brandID
+const addBrandByBrandId = async (req, res) => {
+    const { brand, brandId } = req.params;
+    const newBrandData = req.body;
+};
+
+// Function to delete a brand by brand and brandID
+const deleteBrandByBrandId = async (req, res) => {
+    const { brand, brandId } = req.params;
+};
