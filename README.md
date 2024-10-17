ec55598f64b22e7f33bc3c254601f7d79cb2691b
tiffster2500 <tiff6377@hotmail.com>
2024-10-17 09:39:32 -0600

    added Jonathan to the documentation

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
-# B&V Car Shop Api Documentation
-
+# Team Car Shop Api Documentation
 
 ## Public Render URL
-[B&V Car Shop](https://three41-final.onrender.com)
 
+[Car Shop](https://three41-final.onrender.com)
 
 ## Endpoints
 
-
-
 ## Common Error Responses
+
 - **400 Bad Request**: Invalid input.
 - **404 Not Found**: Resource not found.
 - **500 Internal Server Error**: Unexpected error.
 
 ### Created By
-Tiffany Voorhees & Luke Briggs 
+
+Tiffany Voorhees & Luke Briggs & Jonathan Aloya
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,7 @@ router.use('/', require('./swagger'));
 router.use('/cars', require('./cars'));
 
 router.get('/', (req, res) => {
-  res.send('Welcome to B&V Car Shop! We will have a homepage up soon.');
+  res.send('Welcome to Team Car Shop! We will have a homepage up soon.');
 });
-
-// **separate routes for to diferent collections
-
-// router.use('/ferrari', require('./ferrari'));
-// router.use('/ford', require('./ford'));
-// router.use('/porsche', require('./porsche'));
 
 module.exports = router;
