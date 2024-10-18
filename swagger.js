7cf9b61400cfa71bc190767c4c4bc0a23ad406ff
tiffster2500 <tiff6377@hotmail.com>
2024-10-17 19:44:13 -0600

    return to original swagger setup

--- swagger.js
+++ swagger.js
@@ -1,9 +1,4 @@
 const swaggerAutogen = require('swagger-autogen')();
-// Import 'spawn' from the 'child_process' module
-const { spawn } = require('child_process');
-
-// Set Environment-specific configurations
-const isProduction = process.env.NODE_ENV === 'production';
 
 const doc = {
   info: {
@@ -11,10 +6,8 @@ const doc = {
     description:
       'CSE 341 Final Project for Tiffany Voorhees and Luke Briggs and Jonathan Aloya',
   },
-  host: isProduction
-    ? 'cse341-teamproject-carshop.onrender.com'
-    : 'localhost:3001',
-  schemes: isProduction ? ['https'] : ['http'],
+  host: 'cse341-teamproject-carshop.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
@@ -23,25 +16,4 @@ const routes = ['./routes/index.js'];
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
-swaggerAutogen(outputFile, routes, doc).then(() => {
-  console.log('Swagger documentation generated successfully!');
-
-  //After generating, start the appropriate server
-  const serverCommand = isProduction ? 'node' : 'npx';
-  const serverArgs = isProduction ? ['server.js'] : ['nodemon','server.js'];
-
-  const server = spawn(serverCommand, serverArgs, {
-    stdio: 'inherit', //Pipe the output to the current process
-    shell: true, // Ensures cross-platform compatibility (Windows/Linux/Mac)
-  });
-
-  // Handle any errors in the child process
-  server.on('error', (err) => {
-    console.error(`Failed to start server: ${err.message}`);
-  });
-
-  // Exit event
-  server.on('close', (code) => {
-    console.log(`Server process exited with code ${code}`);
-  });
-});
+swaggerAutogen(outputFile, routes, doc);
